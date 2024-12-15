475bf1b02f61cf3e8b036392bafc58d22e0faf8d
tungb <tungb100203@gmail.com>
2024-12-15 16:26:21 +0700

    check

--- app/src/main/java/com/example/tymexmobileapp/data/api/RetrofitInstance.kt
+++ app/src/main/java/com/example/tymexmobileapp/data/api/RetrofitInstance.kt
@@ -4,12 +4,10 @@ import okhttp3.OkHttpClient
 import okhttp3.Request
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import okhttp3.Interceptor
 import okhttp3.logging.HttpLoggingInterceptor
 
 object RetrofitInstance {
     private const val BASE_URL = "https://api.github.com/"
-//    private const val ACCESS_TOKEN = "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"
 
     private val okHttpClient = OkHttpClient.Builder().apply {
         addInterceptor(HttpLoggingInterceptor().apply {
